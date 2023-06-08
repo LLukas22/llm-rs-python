13e66ae6df8e12d8918b4964eccfe4ad71a5cdd1
Lukas Kreussel <65088241+LLukas22@users.noreply.github.com>
2023-06-08 16:45:32 +0200

    Implemented invoke in RustformersInvocationLayer

--- llm_rs/convert/auto_converter.py
+++ llm_rs/convert/auto_converter.py
@@ -12,7 +12,8 @@ _ARCHITECTURE_CONVERTER_MAP = {
     "GPTJForCausalLM": GptJConverter,
     "GPT2LMHeadModel": Gpt2Converter,
     "BloomForCausalLM":BloomConverter,
-    "LLaMAForCausalLM":LlamaConverter
+    "LLaMAForCausalLM":LlamaConverter,
+    "LlamaForCausalLM":LlamaConverter,
 }
 
 class AutoConverter():
