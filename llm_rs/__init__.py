6e9c3150a2e1fd545e7665b7c9c45a43e2912d4d
Lukas Kreussel <65088241+LLukas22@users.noreply.github.com>
2023-07-17 15:19:10 +0200

    Auto-search cuda dllls

--- llm_rs/__init__.py
+++ llm_rs/__init__.py
@@ -1,4 +1,28 @@
-from .llm_rs import *
+try:
+    from .llm_rs import *
+except ImportError as e:
+    print("DLLs are were not boundled with this package. Trying to locate them...")
+    import os
+    import platform
+
+    #Try to locate CUDA_PATH environment variable
+    cuda_path = os.environ.get("CUDA_PATH",None)
+    if cuda_path:
+        print(f"Found CUDA_PATH environment variable: {cuda_path}")
+        if platform.system() == "Windows":
+            cuda_path = os.path.join(cuda_path,"bin")
+        else:
+            cuda_path = os.path.join(cuda_path,"lib64")
+
+        print(f"Adding {cuda_path} to DLL search path...")
+        os.add_dll_directory(cuda_path)
+        
+    try:
+        from .llm_rs import *
+    except ImportError as inner_e:
+        raise  ImportError("Could not locate DLLs. Please check the documentation for more information.")
+   
+
 
 from .config import GenerationConfig, SessionConfig, Precision, ContainerType, QuantizationType
 from .models import Llama, GptJ, Gpt2, Bloom, GptNeoX, Mpt
